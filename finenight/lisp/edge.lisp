dfcdee8cb0d3a0f20d5580c6576f54195ef03469
Jean-Philippe Barrette-LaPierre <jpb@rrette.com>
2006-04-03 19:55:27 +0000

    [curlpp-svn @ 26] we got a fst-edge now

--- finenight/lisp/edge.lisp
+++ finenight/lisp/edge.lisp
@@ -13,12 +13,24 @@
   symb
   dst)
 
-;;; This function will build an edge from a 3-tuple
-(defmethod build-edge (e)
-  (make-edge
-   :src (edge-source e) 
-   :symb (edge-symbol e) 
-   :dst (edge-destination e)))
+(defstruct fst-edge 
+  src
+  input-symb
+  output-symb
+  dst)
+
+;;; This function will build an edge from a 3-tuple, or a fst-edge
+;;; if it's a 4-tuple.
+(defun build-edge (e)
+  (if (equal (length e) 4)
+      (make-fst-edge
+       :src (edge-source e)
+       :input-symb (edge-input e)
+       :output-symb (edge-output e))
+    (make-edge
+     :src (edge-source e) 
+     :symb (edge-symbol e) 
+     :dst (edge-destination e))))
 
 ;;; This function returns the source state identification
 ;;; of this edge.
@@ -37,6 +49,23 @@
 ;;; This function returns the symbol of this edge.
 (defmethod edge-symbol ((e edge))
   (edge-symb e))
+
+;;; This function returns the symbol (input/output) of the edge
+(defmethod edge-symbol ((e fst-edge))
+  (format t "~A/~A" (fst-edge-input-symb e) (fst-edge-output-symb e)))
+
+(defmethod edge-input ((edge fst-edge))
+  (fst-edge-input-symb edge))
+
+(defmethod edge-input (edge)
+  (edge-symbol edge))
+
+(defmethod edge-output ((edge fst-edge))
+  (fst-edge-output-symb edge))
+
+(defmethod edge-output (edge)
+  (edge-symbol edge))
+
 
 ;;; This function returns the destination state identification
 ;;; of this edge.
