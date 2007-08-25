f2d3a7a5c731adb28bf4c010571a3ad32482c323
Jean-Philippe Barrette-LaPierre <jpb@rrette.com>
2007-08-25 12:58:37 -0400

    better output

--- finenight/lisp/iadfa-run.lisp
+++ finenight/lisp/iadfa-run.lisp
@@ -6,7 +6,7 @@
 (defun iadfa-run ()
   (gen-iadfa-from-file "../../data/com.zone.sorted.small"))
 
-(gen-iadfa-from-file "../../data/com.zone.sorted")
+(gen-iadfa-from-file "../../data/com.zone.sorted.small")
 ;(sb-ext:save-lisp-and-die "iadfa-run" :executable t :toplevel #'iadfa-run)
 
 
@@ -14,4 +14,4 @@
   (setf last-time (get-internal-real-time))
   (sleep 3)
   (setf current-time (get-internal-real-time))
-  (* 1000 (/ 1 (/ (- current-time last-time) internal-time-units-per-second))))
+  (float (* 1000 (/ 1 (/ (- current-time last-time) internal-time-units-per-second)))))
