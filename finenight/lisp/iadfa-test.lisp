a6745ed5a65b8c4f822571038c90b48f0eb084ab
Jean-Philippe Barrette-LaPierre <jpb@rrette.com>
2007-09-04 17:15:32 -0400

    updated the deftests

--- finenight/lisp/iadfa-test.lisp
+++ finenight/lisp/iadfa-test.lisp
@@ -17,96 +17,82 @@
 					    "cappend" "cappendice"
 					    "mormont")))
 
+(defmacro test-equivalence (words)
+  (with-syms (w iadfa output)
+    `(let* ((,w ,words)
+            (,iadfa (debug-gen-iadfa ,w))
+	    (,output nil))
+       (setf ,output (extract-words (iadfa-fsa ,iadfa)))
+       (format t "input:~%~S~%output:~%~S~%" ,w ,output)
+       (equal ,w ,output))))
+       
+
 (defun iadfa-non-branch-suffix ()
   "This tests that the output of the iadfa isn't screwed up 
 by the prefix 0--0 of 0--0--0 because the delete branch 
 don't delete any node"
-  (let* ((words '("0-----0"
-		  "0--0"
-		  "0--0--0"))
-	 (iadfa (debug-gen-iadfa words)))
-    (equal words (extract-words (iadfa-fsa iadfa)))))
+  (test-equivalence '("0-----0"
+		      "0--0"
+		      "0--0--0")))
 
-(org.ancar.CLUnit::deftest "IADFA list"
+(org.ancar.CLUnit::deftest "IADFA tests"
     :category "Destinations" 
     :test-fn #'iadfa-non-branch-suffix)
 
+(defun iadfa-test1 ()
+  (test-equivalence '("0-APR-CREDIT-CARD"
+		      "0-APR-CREDIT-CARD-4U"
+		      "0-APR-CREDIT-CARD-APPLICATION"
+		      "0-APR-CREDIT-CARD-OFFERS"
+		      "0-APR-CREDIT-CARD-ONLINE"
+		      "0-APR-CREDIT-CARDS"
+		      "0-APR-CREDITCARD"
+		      "0-APR-CREDITCARDS"
+		      "0-APR-CREDITS-CARD")))
+(defun iadfa-test2 ()
+ "This situation would cause to have an empty ancestror fsa."
+ (test-equivalence '("0000"
+		     "0001")))
+
+
+
+(defun iadfa-test3 ()
+  "bad behavior where the common-suffix was going too far.
+We had a cycle on the W (1 -W> 2 -O> 3 -O> 1)."
+  (test-equivalence '("08WL"
+		      "08WOOL"
+		      "08WOOOL"
+		      "08WOOOOL")))
+
+(defun iadfa-test4 ()
+  "The stem wasn't long enough."
+  (test-equivalence '("0-adance"
+		      "0-atransfers"
+		      "0-babobibobu"
+		      "0-balance"
+		      "0-balance-transfers")))
+
+
+(org.ancar.CLUnit::deftest "IADFA Test 1"
+    :category "Destinations" 
+    :test-fn #'iadfa-test1)
+
+(org.ancar.CLUnit::deftest "IADFA Test 2"
+    :category "Destinations" 
+    :test-fn #'iadfa-test2)
+
+(org.ancar.CLUnit::deftest "IADFA Test 3"
+    :category "Destinations" 
+    :test-fn #'iadfa-test3)
+
+(org.ancar.CLUnit::deftest "IADFA Test 4"
+    :category "Destinations" 
+    :test-fn #'iadfa-test4)
+
 (org.ancar.CLUnit::run-all-tests)
 
-(defparameter *my-iadfa* (gen-iadfa '("0-APR-CREDIT-CARD"
-				      "0-APR-CREDIT-CARD-4U"
-				      "0-APR-CREDIT-CARD-APPLICATION"
-				      "0-APR-CREDIT-CARD-OFFERS"
-				      "0-APR-CREDIT-CARD-ONLINE"
-				      "0-APR-CREDIT-CARDS"
-				      "0-APR-CREDITCARD"
-				      "0-APR-CREDITCARDS"
-				      "0-APR-CREDITS-CARD")))
-
-(extract-words (iadfa-fsa *my-iadfa*))
-
-;; This situation would cause to have an empty ancestror fsa.
-(defparameter *my-iadfa* (gen-iadfa '("0000"
-					    "0001")))
 
 
-
-;; bad behavior where the common-suffix was going too far.
-;; We had a cycle on the W (1 -W> 2 -O> 3 -O> 1).
-(defparameter *my-iadfa* (gen-iadfa '("08WL"
-					    "08WOOL"
-					    "08WOOOL"
-					    "08WOOOOL")))
-
-;; bad behavior where the common-suffix was going too far.
-;; We had a cycle on the W (1 -W> 2 -O> 3 -O> 1).
-(defparameter *my-iadfa* (gen-iadfa '("0060GIRLSXX"
-					    "0061TEENSX"
-					    "0061TEENSXX")))
-
-;; the stem wasn't long enough).
-(defparameter *my-iadfa* (gen-iadfa '("0-atransfers"
-					    "0-adance"
-					    "0-babobibobu"
-					    "0-balance"
-					    "0-balance-transfers")))
-
-
-(defparameter *my-iadfa* (gen-iadfa '("addendice"
-				      "append" "appendice")))
-
-;(com.rrette.finenight.fsa::save-fsa (iadfa-fsa *my-iadfa*))
-
-;(defparameter *my-fsa* (gen-iadfa-from-file "../../data/test.dico"))
-;(defparameter *my-fsa* (gen-iadfa-from-file "com.zone.sorted.small"))
-;(defparameter *my-fsa* (gen-iadfa-from-file "com.zone.sorted.small"))
-;(graphviz-export-to-file (make-fsa-builder-from-fsa *my-fsa*) "export.dot")
-
-;(defparameter *my-fsa* (iadfa-fsa *my-iadfa*))
-
-
-;; (defun test-dest-1 ()
-;;   (eq (sort (iadfa-state-ancestrors *my-iadfa* 1 #\e) #'<) 
-;;       '(8 22 39)))
-
-;; (defun test-dest-2 ()
-;;   (eq (sort (iadfa-state-ancestrors *my-iadfa* 7 #\a) #'<) 
-;;       '(0)))
-
-;; (defun test-accept ()
-;;   (and (accept? *my-fsa*  "appendice")
-;;        (accept? *my-fsa* "bateau")
-;;        (not (accept? *my-fsa* "appendic"))
-;;        (accept? *my-fsa* "append")))
-
-
-;; (org.ancar.CLUnit::deftest "Destinations 2" 
-;;   :category "Destinations" 
-;; 	    :test-fn #'test-dest-2)
-
-
-;(graphviz-export (make-fsa-builder-from-fsa *my-fsa*))
-;(graphviz-export-to-file (build-fsa-from-ancestrors *my-iadfa*) "ancestrors.dot")
 
 
 
